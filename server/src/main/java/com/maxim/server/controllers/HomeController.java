c7ef5b65907309f0ed56ba6f3b238525306e7bdf
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-12-21 16:55:47 +0200

    optimize imports

--- server/src/main/java/com/maxim/server/controllers/HomeController.java
+++ server/src/main/java/com/maxim/server/controllers/HomeController.java
@@ -1,10 +1,10 @@
 package com.maxim.server.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/")
