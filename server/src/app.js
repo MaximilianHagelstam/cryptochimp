0e45a169a5c353872c344a9013f967e9accae058
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-12-28 12:27:22 +0200

    Add cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
+const cors = require('cors');
 const connectDb = require('./utils/connectDb');
 const personRoutes = require('./routes/personRoutes');
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URI,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  }),
+);
 app.use(morgan('tiny'));
 app.use(helmet());
 
