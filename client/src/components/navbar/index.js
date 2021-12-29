814a020215e4c1eace89bf33d6446d2ee69348bc
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-12-29 12:39:29 +0200

    Add sign up button

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -15,25 +15,23 @@ import {
   useColorModeValue
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import UserService from '../../services/UserService';
 
 const Navbar = () => {
-  const navigate = useNavigate();
   const { colorMode, toggleColorMode } = useColorMode();
   const [user, setUser] = useState({});
+  const [isAuth, setIsAuth] = useState(false);
 
   useEffect(() => {
     (async () => {
       const currentUser = await UserService.getCurrentUser();
 
       if (currentUser) {
+        setIsAuth(true);
         setUser(currentUser);
-      } else {
-        navigate('/james');
       }
     })();
-  }, [navigate]);
+  }, []);
 
   return (
     <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
@@ -46,32 +44,47 @@ const Navbar = () => {
               {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
             </Button>
 
-            <Menu>
-              <MenuButton
-                as={Button}
-                rounded={'full'}
-                variant={'link'}
-                cursor={'pointer'}
-                minW={0}
+            {isAuth ? (
+              <Menu>
+                <MenuButton
+                  as={Button}
+                  rounded={'full'}
+                  variant={'link'}
+                  cursor={'pointer'}
+                  minW={0}
+                >
+                  <Avatar size={'sm'} src={user.avatar} />
+                </MenuButton>
+                <MenuList alignItems={'center'}>
+                  <br />
+                  <Center>
+                    <Avatar size={'2xl'} src={user.avatar} />
+                  </Center>
+                  <br />
+                  <Center>
+                    <p>{user.displayName}</p>
+                  </Center>
+                  <br />
+                  <MenuDivider />
+                  <MenuItem>Account Settings</MenuItem>
+                  <MenuItem>Logout</MenuItem>
+                </MenuList>
+              </Menu>
+            ) : (
+              <Button
+                display={{ base: 'none', md: 'inline-flex' }}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'pink.400'}
+                href={'#'}
+                _hover={{
+                  bg: 'pink.300'
+                }}
               >
-                <Avatar size={'sm'} src={user.avatar} />
-              </MenuButton>
-              <MenuList alignItems={'center'}>
-                <br />
-                <Center>
-                  <Avatar size={'2xl'} src={user.avatar} />
-                </Center>
-                <br />
-                <Center>
-                  <p>{user.displayName}</p>
-                </Center>
-                <br />
-                <MenuDivider />
-                <MenuItem>Your Servers</MenuItem>
-                <MenuItem>Account Settings</MenuItem>
-                <MenuItem>Logout</MenuItem>
-              </MenuList>
-            </Menu>
+                Sign Up
+              </Button>
+            )}
           </Stack>
         </Flex>
       </Flex>
