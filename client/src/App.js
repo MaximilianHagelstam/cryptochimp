3a029f0bc6e219b423e9f3f689b21e6276a893ff
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-12-29 20:10:03 +0200

    Add navbar on all components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router-dom';
+import Home from './components/home';
 import Navbar from './components/navbar';
 import NotFound from './components/notFound';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="*" element={<NotFound />} />
-      <Route path="/" element={<Navbar />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ client/src/components/home/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <p>James</p>;
+};
+
+export default Home;
