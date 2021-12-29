69c87b744f3c3bf9c742b0d6b1991b444eccdf7b
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2021-12-29 13:44:34 +0200

    Add 404 page

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { render } from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
+import NotFound from './components/notFound';
 
 render(
   <BrowserRouter>
     <ChakraProvider>
       <Routes>
+        <Route path="*" element={<NotFound />} />
         <Route path="/" element={<Navbar />} />
       </Routes>
     </ChakraProvider>
