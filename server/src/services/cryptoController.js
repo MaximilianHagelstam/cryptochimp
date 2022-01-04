1001fdc8ca2dd06702fdcffefb55ea373cb53985
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-04 11:50:37 +0200

    Add query param

--- server/src/services/cryptoController.js
+++ server/src/services/cryptoController.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 
 const BASE_URL = 'https://pro-api.coinmarketcap.com/v1';
 
-const getAllCoins = async (_req, res) => {
+const getAllCoins = async (req, res) => {
   try {
+    const { limit } = req.query;
     const { data } = await axios.get(
-      `${BASE_URL}/cryptocurrency/listings/latest?start=1&limit=10&convert=USD`,
+      `${BASE_URL}/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=USD`,
       {
         headers: {
           'X-CMC_PRO_API_KEY': process.env.COINMARKET_API_KEY
