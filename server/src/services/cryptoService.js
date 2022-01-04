6979ff15921801f8759dc60f7361ac5438a4f1df
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-04 21:42:56 +0200

    Switch to ocinmarket api for price data

--- server/src/services/cryptoService.js
+++ server/src/services/cryptoService.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
-const CoinGecko = require('coingecko-api');
-
-const CoinGeckoClient = new CoinGecko();
 
 const BASE_URL = 'https://pro-api.coinmarketcap.com/v1';
 
 const getAllCoins = async (req, res) => {
   try {
     const { limit } = req.query;
+
     const { data } = await axios.get(
       `${BASE_URL}/cryptocurrency/listings/latest?start=1&limit=${limit}&convert=USD`,
       {
@@ -24,25 +22,20 @@ const getAllCoins = async (req, res) => {
 
 const getPrice = async (req, res) => {
   try {
-    const { coin } = req.query;
+    const { symbol } = req.query;
 
-    const { data } = await CoinGeckoClient.coins.fetch(coin, {
-      tickers: false,
-      community_data: false,
-      developer_data: false,
-      localization: false
-    });
+    const { data } = await axios.get(
+      `${BASE_URL}/cryptocurrency/quotes/latest?symbol=${symbol}`,
+      {
+        headers: {
+          'X-CMC_PRO_API_KEY': process.env.COINMARKET_API_KEY
+        }
+      }
+    );
 
-    const responseObject = {
-      id: data.id,
-      symbol: data.symbol,
-      name: data.name,
-      current_price_usd: data.market_data.current_price.usd
-    };
-
-    res.send(responseObject);
+    res.send(data.data);
   } catch (err) {
-    res.send({ error: 'Error fetching CoinGecko API', message: err });
+    res.send({ error: 'Error fetching CoinMarketCap API', message: err });
   }
 };
 
