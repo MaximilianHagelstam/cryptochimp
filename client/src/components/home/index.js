410c8f897deac486bc27bdc8deb94ec6af9016d7
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-04 12:10:00 +0200

    Get coins from client

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,5 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const Home = () => {
-  return <p>James</p>;
+  const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [perPage, setPerPage] = useState(5);
+
+  const URL = `${process.env.REACT_APP_API_URL}/api/crypto/all?limit=${perPage}`;
+
+  useEffect(() => {
+    (async () => {
+      setLoading(true);
+      const res = await axios.get(URL);
+      // setCoins([...coins, ...res]);
+      setLoading(false);
+
+      console.log(res.data);
+    })();
+  }, [URL]);
+
+  return (
+    <>
+      <p>James</p>
+    </>
+  );
 };
 
 export default Home;
