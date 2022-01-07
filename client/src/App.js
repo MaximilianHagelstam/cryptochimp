2b7eaeb205c554aa447d649c5c1376d58c52ac44
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-07 17:18:58 +0200

    Test

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Buy from './components/buy';
 import Home from './components/home';
-import Login from './components/login';
 import Navbar from './components/navbar';
 import NotFound from './components/notFound';
 import Sell from './components/sell';
@@ -24,20 +23,14 @@ const App = () => {
 
   return (
     <>
-      {isAuth ? (
-        <>
-          <Navbar />
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/buy" element={<Buy />} />
-            <Route path="/sell" element={<Sell />} />
-            <Route path="/wallet" element={<Wallet />} />
-          </Routes>
-        </>
-      ) : (
-        <Login />
-      )}
+      <Navbar />
+      <Routes>
+        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/buy" element={<Buy />} />
+        <Route path="/sell" element={<Sell />} />
+        <Route path="/wallet" element={<Wallet />} />
+      </Routes>
     </>
   );
 };
