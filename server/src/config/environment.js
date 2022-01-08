f0b81b0de64913b0b6a91d96f8192651f1fb5c30
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-08 10:52:30 +0200

    Bäkis

--- server/src/config/environment.js
+++ server/src/config/environment.js
@@ -5,8 +5,8 @@ dotenv.config();
 const PORT = process.env.PORT || 8080;
 
 const MONGO_URI =
-  process.env.NODE_ENV === 'production'
-    ? process.env.MONGO_URI_PROD
-    : process.env.MONGO_URI_DEV;
+  process.env.NODE_ENV === 'development'
+    ? process.env.MONGO_URI_DEV
+    : process.env.MONGO_URI_PROD;
 
 module.exports = { PORT, MONGO_URI };
