04be6c9cf062ee8e0eb37613aaf06ae508826656
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-01-20 18:37:31 +0200

    Improve healthCheck test

---
+++ server/test/healthCheck.test.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../src/app');
+
+const api = supertest(app);
+
+describe('GET / should', () => {
+  test('return json message with code 200', async () => {
+    const res = await api.get('/');
+
+    console.log('asdfasdf');
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.type).toEqual('application/json');
+    expect(res.body.message).toEqual('Server running');
+  });
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
