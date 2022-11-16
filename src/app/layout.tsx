74e87ad9c4661e2a2bed3e0bc05dde2fdd306b20
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-11-16 10:57:59 +0200

    Setup eslint, prettier and tailwind

---
+++ src/app/layout.tsx
@@ -0,0 +1,12 @@
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: RootLayoutProps) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <h1 className="font-extrabold text-green-400">CryptoChimp</h1>;
+}
