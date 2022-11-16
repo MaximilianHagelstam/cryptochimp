992a126a38a71ffce300699763ff66d19db2da81
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-11-16 19:35:00 +0200

    Fix tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import '../styles/globals.css';
+
 interface RootLayoutProps {
   children: React.ReactNode;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <h1 className="font-extrabold text-green-400">CryptoChimp</h1>;
+  return (
+    <h1 className="text-7xl font-extrabold text-green-500">CryptoChimp</h1>
+  );
 }
