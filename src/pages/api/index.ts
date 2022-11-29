078544722e288274ca855f4dc216eb70f4d80ef1
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2022-11-29 12:19:29 +0200

    Add api endpoint

---
+++ src/pages/api/index.ts
@@ -0,0 +1,15 @@
+import type { User } from '@prisma/client';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../lib/prisma';
+
+type Data = {
+  users: User[];
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  const users = await prisma.user.findMany({});
+  res.status(200).json({ users });
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,14 @@
+import type { User } from '@prisma/client';
+import useSWR from 'swr';
+
 export default function Home() {
-  return <button className="btn-primary btn">Hello Team</button>;
+  const { data } = useSWR<User[]>('/api', (url) =>
+    fetch(url).then((res) => res.json())
+  );
+  return (
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <button className="btn-primary btn">Hello Team</button>
+    </>
+  );
 }
