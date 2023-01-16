a2c735d83ffe539504ef756963a2998a927e2492
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-01-16 19:38:47 +0200

    Design dashboard layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
-import { type NextPage } from "next";
+import type { NextPage } from "next";
+import { Block, Card, Col, ColGrid } from "@tremor/react";
 
-const Home: NextPage = () => {
-  return <h1>Hello team!</h1>;
+const Dashboard: NextPage = () => {
+  return (
+    <>
+      <ColGrid
+        numColsMd={2}
+        numColsLg={3}
+        gapX="gap-x-6"
+        gapY="gap-y-6"
+        marginTop="mt-6"
+      >
+        <Card>
+          <div className="h-28" />
+        </Card>
+        <Card>
+          <div className="h-28" />
+        </Card>
+        <Card>
+          <div className="h-28" />
+        </Card>
+      </ColGrid>
+      <Block marginTop="mt-6">
+        <ColGrid numColsLg={6} gapX="gap-x-6" gapY="gap-y-6" marginTop="mt-6">
+          <Col numColSpanLg={4}>
+            <Card hFull={true}>
+              <div className="h-60" />
+            </Card>
+          </Col>
+          <Col numColSpanLg={2}>
+            <Block spaceY="space-y-6">
+              <Card>
+                <div className="h-96" />
+              </Card>
+            </Block>
+          </Col>
+        </ColGrid>
+      </Block>
+    </>
+  );
 };
 
-export default Home;
+export default Dashboard;
