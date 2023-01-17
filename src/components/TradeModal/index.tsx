bc3907d656f09ee61fb58f4e102695781b179188
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-01-17 16:35:24 +0200

    Improve trade confirmation modal design

--- src/components/TradeModal/index.tsx
+++ src/components/TradeModal/index.tsx
@@ -44,71 +44,69 @@ const TradeModal = ({
       <Dialog as="div" className="relative z-10" onClose={closeModal}>
         <Transition.Child
           as={Fragment}
-          enter="ease-out duration-300"
+          enter="ease-out duration-100"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="ease-in duration-200"
+          leave="ease-in duration-100"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-slate-500 bg-opacity-75 transition-opacity" />
+          <div className="fixed inset-0 bg-black bg-opacity-50" />
         </Transition.Child>
 
-        <div className="fixed inset-0 z-10 overflow-y-auto">
-          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <div className="fixed inset-0 z-10 overflow-y-auto px-2">
+          <div className="flex min-h-full items-center justify-center text-center">
             <Transition.Child
               as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-              enterTo="opacity-100 translate-y-0 sm:scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
-              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enter="ease-out duration-00"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="ease-in duration-100"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
-                <div className="px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                  <div className="sm:flex sm:items-start">
-                    <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                      <Dialog.Title
-                        as="h3"
-                        className="text-lg font-medium leading-6"
-                      >
-                        {t.trade.confirmOrder}
-                      </Dialog.Title>
-                      <div className="mt-2">
-                        <List>
-                          <ListItem>
-                            <span>{t.common.coin}</span>
-                            <span>{symbol}</span>
-                          </ListItem>
-                          <ListItem>
-                            <span>{t.common.amount}</span>
-                            <span>{amount}</span>
-                          </ListItem>
-                          <ListItem>
-                            <span>{t.common.type}</span>
-                            <Badge
-                              text={
-                                type === "BUY"
-                                  ? t.common.buy.toUpperCase()
-                                  : t.common.sell.toUpperCase()
-                              }
-                              color={type === "BUY" ? "blue" : "pink"}
-                              size="xs"
-                            />
-                          </ListItem>
-                        </List>
-                      </div>
+              <Dialog.Panel className="relative w-full max-w-sm transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all">
+                <div className="px-8 pt-4 pb-4">
+                  <div className="mt-2 text-center">
+                    <Dialog.Title
+                      as="h3"
+                      className="text-xl font-medium leading-6"
+                    >
+                      {t.trade.confirmOrder}
+                    </Dialog.Title>
+                    <div className="mt-4">
+                      <List>
+                        <ListItem>
+                          <span>{t.common.coin}</span>
+                          <span>{symbol}</span>
+                        </ListItem>
+                        <ListItem>
+                          <span>{t.common.amount}</span>
+                          <span>{amount}</span>
+                        </ListItem>
+                        <ListItem>
+                          <span>{t.common.type}</span>
+                          <Badge
+                            text={
+                              type === "BUY"
+                                ? t.common.buy.toUpperCase()
+                                : t.common.sell.toUpperCase()
+                            }
+                            color={type === "BUY" ? "blue" : "pink"}
+                            size="xs"
+                          />
+                        </ListItem>
+                      </List>
                     </div>
                   </div>
                 </div>
-                <div className="border-t border-slate-200 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <div className="border-t border-slate-200 px-4 py-3">
                   <button
                     className={classNames(
                       type === "BUY"
                         ? "bg-blue-600 hover:bg-blue-700"
                         : "bg-pink-600 hover:bg-pink-700",
-                      "inline-flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none sm:ml-3 sm:w-auto sm:text-sm"
+                      "inline-flex w-full justify-center rounded-md border border-transparent px-4 py-2 text-base font-medium text-white shadow-sm focus:outline-none"
                     )}
                     onClick={handleConfirm}
                     disabled={isLoading}
@@ -116,7 +114,7 @@ const TradeModal = ({
                     {type === "BUY" ? t.common.buy : t.common.sell}
                   </button>
                   <button
-                    className="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-base font-medium text-gray-700 shadow-sm hover:bg-gray-50 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
+                    className="mt-3 inline-flex w-full justify-center rounded-md border border-slate-300 bg-white px-4 py-2 text-base font-medium text-slate-700 shadow-sm hover:bg-slate-100"
                     onClick={closeModal}
                   >
                     {t.trade.cancel}
