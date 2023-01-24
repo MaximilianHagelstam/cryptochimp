81ac283f42f697bc565f99fd726ebb52e90b0ce5
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-01-25 00:26:20 +0200

    Add pagination to market data table

--- src/pages/market.tsx
+++ src/pages/market.tsx
@@ -1,14 +1,21 @@
 import type { NextPage } from "next";
-import { Card } from "@tremor/react";
+import { useState } from "react";
+import { Button, Card, Flex, Footer } from "@tremor/react";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
 import { trpc } from "../utils/trpc";
+import { useTranslation } from "../hooks/useTranslation";
 import ErrorPage from "../components/ErrorPage";
 import MarketDataTable from "../components/MarketDataTable";
 
+const LIMIT = 100;
+
 const Market: NextPage = () => {
+  const { t } = useTranslation();
+  const [start, setStart] = useState(1);
   const { data, isError, isLoading, error } =
     trpc.market.getMarketData.useQuery({
-      start: 1,
-      limit: 100,
+      start,
+      limit: LIMIT,
     });
 
   if (isLoading)
@@ -21,6 +28,29 @@ const Market: NextPage = () => {
   return (
     <Card>
       <MarketDataTable coins={data} />
+
+      <Footer height="h-16">
+        <Flex justifyContent="justify-end" spaceX="space-x-2">
+          <Button
+            text={t.transactions.previous}
+            size="sm"
+            variant="secondary"
+            icon={ChevronLeftIcon}
+            iconPosition="left"
+            onClick={() => setStart((prev) => prev - LIMIT)}
+            disabled={start === 1}
+          />
+          <Button
+            text={t.transactions.next}
+            variant="secondary"
+            size="sm"
+            disabled={data.length < LIMIT}
+            icon={ChevronRightIcon}
+            iconPosition="right"
+            onClick={() => setStart((prev) => prev + LIMIT)}
+          />
+        </Flex>
+      </Footer>
     </Card>
   );
 };
