f54634b1ad9f958ab909885dc7b81997d2387f60
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2023-06-21 16:37:41 +0300

    Update to Tremor 3

--- next.config.js
+++ next.config.js
@@ -1,11 +1,5 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  swcMinify: true,
-  experimental: {
-    appDir: true,
-    serverComponentsExternalPackages: ["@tremor/react"],
-  },
   images: {
     domains: ["lh3.googleusercontent.com"],
   },
--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,9 +1,13 @@
 import { TransactionsTable } from "@/components/TransactionsTable";
 import { getTransactions } from "@/lib/api";
 import { getUserId } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export default async function Transactions() {
   const userId = await getUserId();
+  if (!userId) {
+    redirect("/api/auth/signin");
+  }
   const transactions = await getTransactions(userId);
 
   return <TransactionsTable transactions={transactions} />;
