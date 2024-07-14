d94aedd59bea04856f0880445c297c938898ff2e
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-07-14 11:40:06 +0300

    refactor: put constants into a single file

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -1,12 +1,13 @@
 import { getTransactions } from "@/lib/api";
 import { getCurrentUser } from "@/lib/auth";
+import { LOGIN_URL } from "@/lib/constants";
 import { redirect } from "next/navigation";
 import { TransactionsTable } from "./TransactionsTable";
 
 export default async function Transactions() {
   const user = await getCurrentUser();
   if (!user) {
-    redirect("/api/auth/signin");
+    redirect(LOGIN_URL);
   }
 
   const transactions = await getTransactions(user.id);
---
+++ src/lib/constants.ts
@@ -0,0 +1,5 @@
+export const INITIAL_CAPITAL = 10_000;
+
+export const LOGIN_URL = "/api/auth/signin";
+
+export const IS_PROD = process.env.NODE_ENV === "production";
