a30dfab48c33aea3ab44c5e872a5d665b6d50519
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-07-18 21:35:53 +0300

    style: improve trade card

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -13,13 +13,14 @@ import {
   Select,
   SelectItem,
   TextInput,
+  Title,
 } from "@tremor/react";
 import { useFormState, useFormStatus } from "react-dom";
 
 const SubmitButton = () => {
   const { pending } = useFormStatus();
   return (
-    <Button size="xl" type="submit" className="w-full" loading={pending}>
+    <Button size="xl" type="submit" loading={pending}>
       Confirm trade
     </Button>
   );
@@ -32,69 +33,64 @@ export default function Trade() {
   });
 
   return (
-    <div className="flex w-full justify-center">
-      <Card className="max-w-xl">
-        <form action={formAction} className="my-4 px-2 sm:px-16">
-          <div className="flex w-full flex-col items-center justify-center space-y-4">
-            {state.isError && (
-              <div className="w-full">
-                <Callout
-                  title="Error"
-                  icon={ExclamationTriangleIcon}
-                  color="red"
-                >
-                  {state.message}
-                </Callout>
-              </div>
-            )}
+    <Card className="flex w-full justify-center py-12">
+      <div className="flex w-full max-w-xl flex-col justify-center gap-6">
+        <Title>Trade</Title>
+        <form action={formAction} className="flex flex-col gap-3">
+          {state.isError && (
             <div className="w-full">
-              <Bold className="font-medium">Coin</Bold>
-              <TextInput
-                className="mt-2"
-                placeholder="ETH"
-                required
-                maxLength={5}
-                id="symbol"
-                name="symbol"
-              />
+              <Callout title="Error" icon={ExclamationTriangleIcon} color="red">
+                {state.message}
+              </Callout>
             </div>
-            <div className="w-full">
-              <Bold className="font-medium">Quantity</Bold>
-              <NumberInput
-                className="mt-2"
-                min={1}
-                max={1_000_000_000}
-                required
-                id="quantity"
-                name="quantity"
-              />
-            </div>
-            <div className="w-full">
-              <Bold className="font-medium">Type</Bold>
-              <Select
-                id="type"
-                name="type"
-                defaultValue="BUY"
-                enableClear={false}
-                className="mt-2"
-              >
-                <SelectItem value="BUY">
-                  <Badge size="xs" color="blue">
-                    BUY
-                  </Badge>
-                </SelectItem>
-                <SelectItem value="SELL">
-                  <Badge size="xs" color="pink">
-                    SELL
-                  </Badge>
-                </SelectItem>
-              </Select>
-            </div>
+          )}
+          <div>
+            <Bold className="font-medium">Coin</Bold>
+            <TextInput
+              className="mt-2"
+              placeholder="ETH"
+              required
+              maxLength={5}
+              id="symbol"
+              name="symbol"
+            />
+          </div>
+          <div>
+            <Bold className="font-medium">Quantity</Bold>
+            <NumberInput
+              className="mt-2"
+              min={1}
+              max={1_000_000_000}
+              required
+              id="quantity"
+              name="quantity"
+            />
+          </div>
+          <div>
+            <Bold className="font-medium">Type</Bold>
+            <Select
+              id="type"
+              name="type"
+              defaultValue="BUY"
+              enableClear={false}
+              className="mt-2"
+            >
+              <SelectItem value="BUY">
+                <Badge size="xs" color="blue">
+                  BUY
+                </Badge>
+              </SelectItem>
+              <SelectItem value="SELL">
+                <Badge size="xs" color="pink">
+                  SELL
+                </Badge>
+              </SelectItem>
+            </Select>
           </div>
           <Divider className="w-1/3" />
           <SubmitButton />
         </form>
-      </Card>
-    </div>
+      </div>
+    </Card>
   );
 }
