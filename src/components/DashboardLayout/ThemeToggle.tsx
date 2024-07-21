83afc57a3fd8962b833cdf10b352faf4c7da5e33
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-07-21 15:21:09 +0300

    fix: console warning

--- src/components/DashboardLayout/ThemeToggle.tsx
+++ src/components/DashboardLayout/ThemeToggle.tsx
@@ -1,21 +1,33 @@
 "use client";
 
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
+import clsx from "clsx";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 export const ThemeToggle = () => {
+  const [mounted, setMounted] = useState(false);
   const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <button
       className="rounded-full p-2 hover:bg-gray-100 dark:hover:bg-gray-800"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      {theme === "light" ? (
-        <MoonIcon className="h-5 w-5" />
-      ) : (
-        <SunIcon className="h-5 w-5" />
-      )}
+      <MoonIcon
+        className={clsx(theme === "light" ? "block" : "hidden", "h-5 w-5")}
+      />
+      <SunIcon
+        className={clsx(theme === "dark" ? "block" : "hidden", "h-5 w-5")}
+      />
     </button>
   );
 };
