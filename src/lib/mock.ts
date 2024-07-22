49466e053d5dd2579eede068fd5931d418f44645
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-07-23 00:16:50 +0300

    refactor: mock crypto lib

--- src/lib/mock.ts
+++ src/lib/mock.ts
@@ -1,59 +1,55 @@
-import { Coin, DashboardData } from "@/types";
+import { CoinMetadata } from "@/types";
 
-export const getTopCoinsMockData = (length: number): Coin[] => {
-  return new Array(length).fill(null).map((_, index) => ({
+export const getLatest = async (limit: number) => {
+  return new Array(limit).fill(null).map((_, index) => ({
     name: "Bitcoin",
     symbol: "BTC",
-    rank: index + 1,
-    price: 40_000,
-    percentChange1h: 0,
-    percentChange24h: -2,
-    percentChange7d: 4,
-    marketCap: 700_000_000_000,
-    circulatingSupply: 19_000_000,
-    volume24h: 8_000_000,
-    metadata: {
-      logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
-      urls: {
-        source_code: [],
-        reddit: [],
-        website: ["https://bitcoin.org/"],
+    cmc_rank: index,
+    circulating_supply: 19_000_000,
+    quote: {
+      EUR: {
+        price: 40_000,
+        percent_change_1h: 2,
+        percent_change_24h: 5,
+        percent_change_7d: 10,
+        volume_24h: 8_000_000,
+        market_cap: 700_000_000_000,
       },
     },
   }));
 };
 
-export const getDashboardMockData = (): DashboardData => {
-  const currentDate = new Date();
-  return {
-    balance: 1_000,
-    capital: {
-      value: 14_000,
-      percentageChange: 4,
-    },
-    capitalToday: {
-      value: 500,
-      percentageChange: 2,
-    },
-    ownedCoins: new Array(3).fill({
-      name: "Bitcoin",
-      symbol: "BTC",
-      quantity: 3,
-      currentPrice: 30_000,
-      percentChange1h: 0,
-      percentChange24h: -2,
-      percentChange7d: 4,
-      totalValue: 90_000,
+export const getPrice = async (symbol: string = "") => {
+  if (symbol === "BTC") return 40_000;
+  return 100;
+};
+
+export const getMetadata = async (symbols: string[]) => {
+  return symbols.reduce(
+    (acc, symbol) => ({
+      ...acc,
+      [symbol]: [
+        {
+          logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
+          urls: {
+            source_code: [],
+            reddit: [],
+            website: ["https://bitcoin.org/"],
+          },
+        },
+      ],
     }),
-    capitalDataPoints: new Array(10)
-      .fill(null)
-      .map((_, index) => ({
-        id: "",
-        userId: "",
-        capital: Math.floor(Math.random() * (15000 - 5000 + 1)) + 5000,
-        createdAt: new Date(currentDate.setDate(currentDate.getDate() - index)),
-      }))
-      .sort((a, b) => a.createdAt.getTime() - b.createdAt.getTime()),
-    coinCapitalValue: 13_000,
-  };
+    {} as Record<string, Array<CoinMetadata>>
+  );
+
+  // return symbols.map((symbol) => ({
+  //   [symbol]: {
+  //     logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Bitcoin.svg/1200px-Bitcoin.svg.png",
+  //     urls: {
+  //       source_code: [],
+  //       reddit: [],
+  //       website: ["https://bitcoin.org/"],
+  //     },
+  //   },
+  // }));
 };
