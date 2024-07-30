920217013fbc0df6fa9be8a938a9804fbadebcd3
Maximilian Hagelstam <maximilian.hagelstam@gmail.com>
2024-07-30 16:44:02 +0300

    test: add unit tests for util functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -11,15 +11,17 @@ export const formatCurrency = (value: number) =>
 export const formatPercentage = (value: number) => `${value.toFixed(1)}%`;
 
 export const getDeltaType = (percentChange: number) => {
-  const fixedPercentChange = Number(percentChange.toFixed(1));
-  if (fixedPercentChange >= -0.1 && fixedPercentChange <= 0.1)
-    return "unchanged";
-
+  if (percentChange >= -0.1 && percentChange <= 0.1) return "unchanged";
   return percentChange < 0 ? "moderateDecrease" : "moderateIncrease";
 };
 
-export const calculateCoinShare = (coinValue: number, portfolioValue: number) =>
-  (coinValue / portfolioValue) * 100;
+export const calculateCoinShare = (
+  coinValue: number,
+  portfolioValue: number
+) => {
+  if (portfolioValue === 0) return 0;
+  return (coinValue / portfolioValue) * 100;
+};
 
 export const getOwnedCoins = (transactions: Transaction[]) => {
   const uniqueSymbols = [
