aeffb10700ebaecadb37ebef1f08dcc16127ea3f
Maximilian Hagelstam <maximilian.hagelstam@smartum.fi>
2024-11-09 12:52:11 +0200

    chore: bump deps

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
+import { type ThemeProviderProps } from "next-themes";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { ThemeProviderProps } from "next-themes/dist/types";
 
 export const ThemeProvider = ({ children, ...props }: ThemeProviderProps) => {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
